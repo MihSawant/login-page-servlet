03390bf201303057b9ac43e768d4e7ea9604f9bd
Mihir Sawant <mihirssawant06@gmail.com>
2023-04-01 21:13:18 +0530

    pojo added

---
+++ src/main/java/sawant/mihir/loginservlet/entities/Employee.java
@@ -0,0 +1,32 @@
+package sawant.mihir.loginservlet.entities;
+
+public class Employee {
+    private String userName;
+    private String password;
+
+    public Employee(){};
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
